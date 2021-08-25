fc7070cbb33feeea9faa1d19984baf6f8be61f71
Meztli <meztli.valeriano@gmail.com>
2021-08-24 21:24:16 -0500

    mi primer commit

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HolaMundo = () => {
+    return ( 
+        <div>
+            <h1>Hola Mundo soy Meztli3</h1>;
+        </div>
+    );
+}
+
+export default HolaMundo;
